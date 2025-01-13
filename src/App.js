b60504df1bbc541682c82091b2742499d06a6c0d
Bannnerszx <131832924+Bannnerszx@users.noreply.github.com>
2025-01-13 14:39:14 +0900

    Initial commit ssr

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home'; // Ensure you have these components created
 import About from './About';
+import AppEntry from './react-native/AppEntry';
 
 const App = () => (
   <Router>
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<AppEntry />} />
+        {/* <Route path="/about" element={<About />} /> */}
       </Routes>
     </div>
   </Router>
---
+++ src/react-native/AppEntry.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from 'react-native'
+
+const AppEntry = () => {
+    return (
+        <View style={{ justifyContent: 'center' }}>
+            <Text style={{ fontSize: '2em' }}>Hello from client</Text>
+        </View>
+    )
+}
+
+export default AppEntry;
